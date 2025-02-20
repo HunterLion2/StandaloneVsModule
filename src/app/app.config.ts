f33d28978718be9e959181355736ca567e06e420
Çınar Özgürgil <cozgurgil@gmail.com>
2025-02-20 19:42:08 +0300

    standalone Yapısı

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ // Aslında buraya eklediğimiz değerler eski versiyonlarda module klasörüne eklediğimiz module lerdir onları buraya ekleriz.
+    // Ve ekleme yaparken de artık başına provide koyup ekleriz.
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+  ]
 };
